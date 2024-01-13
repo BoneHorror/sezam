0e95646b21dcfa5c6823bab5d4fd8ccd097b1bf3
Veeq7 <rjurkiewicz7@gmail.com>
2024-01-13 09:28:11 +0100

    Wrap window into class and define a clear entry point

--- window.py
+++ window.py
@@ -9,62 +9,66 @@ import os
 
 LOGFILE = "sezam.log"
 
-# TODO: supply intelligent defaults
-local_samase = ""
-unpack_samase = ""
-
 def pathSet(value: str) -> bool:
     if value == "":
         print("Attempted to perform an operation without a directory selected!")
         return False
     return True
-    
-# Log setup
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename=LOGFILE, encoding='utf8', level = logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-logging.FileHandler(LOGFILE, mode = "w")
 
-# Window layout
-layout = [[gui.Button("Select build directory"), gui.Text("Please select a path", key="curPath"), gui.Push(), gui.Checkbox("Run in x64", key="x64")],
-          [gui.Push(), gui.Button("Launch"), gui.Button("Build From"), gui.Push()],
-          [gui.Push()],
-          [gui.Button("Select .exe to Unpack"), gui.Text("No exe selected", key="curExe")],
-          [gui.Push(), gui.Button("Unpack from"), gui.Push()]
-]
+class Window():
+    def __init__(self):
+        # TODO: supply intelligent defaults
+        self.local_samase = ""
+        self.unpack_samase = ""
 
-window = gui.Window("Sezam", layout, resizable=True)
-while True:
-    event, values = window.read()
-    if event == gui.WIN_CLOSED:
-        break
-    if event == "Select build directory":
-        directory = gui.popup_get_folder('Select path', initial_folder=os.getcwd())
-        if directory == "":
-            print("Nothing selected")
-            continue
-        clean_dir = directory[(directory.rfind("/")+1):] # TODO: use appropriate function from os.path
-        window["curPath"].update(value=clean_dir)
-        local_samase = directory
-    if event == "Select Executable to Unpack":
-        directory = gui.popup_get_file('Select path', initial_folder=os.getcwd(), file_types=(("Executable files", "*.exe"),))
-        if directory == "":
-            print("Nothing selected")
-            continue
-        clean_dir = directory[(directory.rfind("/")+1):]
-        window["curExe"].update(value=clean_dir)
-        unpack_samase = directory
-    if event == "Launch":
-        if not pathSet(local_samase):
-            continue
-        samase.run(local_samase, values["x64"])
-    if event == "Build From":
-        if not pathSet(local_samase):
-            continue
-        samase.build(local_samase, values["x64"])
-    if event == "Unpack from":
-        if not pathSet(unpack_samase):
-            continue
-        # on assumption that the user must select .exe file in the popup
-        assert unpack_samase.endswith(".exe"), f"Expected executable to unpack, instead got {local_samase}"
-        samase.build(unpack_samase, values["x64"])
+        layout = [[gui.Button("Select build directory"), gui.Text("Please select a path", key="curPath"), gui.Push(), gui.Checkbox("Run in x64", key="x64")],
+                [gui.Push(), gui.Button("Launch"), gui.Button("Build From"), gui.Push()],
+                [gui.Push()],
+                [gui.Button("Select .exe to Unpack"), gui.Text("No exe selected", key="curExe")],
+                [gui.Push(), gui.Button("Unpack from"), gui.Push()]
+        ]
+        self.window = gui.Window("Sezam", layout, resizable=True)
 
+    def run(self):
+        while True:
+            event, values = self.window.read()
+            if event == gui.WIN_CLOSED:
+                break
+            if event == "Select build directory":
+                directory = gui.popup_get_folder('Select path', initial_folder=os.getcwd())
+                if directory == "":
+                    print("Nothing selected")
+                    continue
+                clean_dir = directory[(directory.rfind("/")+1):] # TODO: use appropriate function from os.path
+                self.window["curPath"].update(value=clean_dir)
+                self.local_samase = directory
+            if event == "Select Executable to Unpack":
+                directory = gui.popup_get_file('Select path', initial_folder=os.getcwd(), file_types=(("Executable files", "*.exe"),))
+                if directory == "":
+                    print("Nothing selected")
+                    continue
+                clean_dir = directory[(directory.rfind("/")+1):]
+                self.window["curExe"].update(value=clean_dir)
+                self.unpack_samase = directory
+            if event == "Launch":
+                if not pathSet(self.local_samase):
+                    continue
+                samase.run(self.local_samase, values["x64"])
+            if event == "Build From":
+                if not pathSet(self.local_samase):
+                    continue
+                samase.build(self.local_samase, values["x64"])
+            if event == "Unpack from":
+                if not pathSet(self.unpack_samase):
+                    continue
+                # on assumption that the user must select .exe file in the popup
+                assert self.unpack_samase.endswith(".exe"), f"Expected executable to unpack, instead got {self.local_samase}"
+                samase.build(self.unpack_samase, values["x64"])
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename=LOGFILE, encoding='utf8', level = logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.FileHandler(LOGFILE, mode = "w")
+
+    window = Window()
+    window.run()
     
