ceea9268ec424e2598d157bc71d25ed38b08ea55
Veeq7 <rjurkiewicz7@gmail.com>
2024-01-13 09:08:26 +0100

    Initial tidy

--- window.py
+++ window.py
@@ -3,30 +3,25 @@
 #pyinstaller window.spec
 import PySimpleGUI as gui
 import logging
-from samase import Samase
+from samase import Samase, OpType
 import os
 
 LOGFILE = "sezam.log"
 
-def exists_localSamase(locals: dict) -> bool:
-    if "localSamase" not in locals:
-            print("Attempted to perform an operation without a directory selected!")
-            return False
-    else:
-        return True
+localSamase = None
+unpackSamase = None
 
-def exists_unpackSamase(locals: dict) -> bool:
-    if "unpackSamase" not in locals:
-            print("Attempted to perform an operation without a directory selected!")
-            return False
-    else:
-        return True
+def assertSet(value: any) -> bool:
+    if value is None:
+        print("Attempted to perform an operation without a directory selected!")
+        return False
+    return True
     
-#log setup
+# Log setup
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename=LOGFILE, encoding='utf8', level = logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 logging.FileHandler(LOGFILE, mode = "w")
 
-#window layout
+# Window layout
 layout = [[gui.Button("Select build directory"), gui.Text("Please select a path", key="curPath"), gui.Push(), gui.Checkbox("Run in x64", key="x64")],
           [gui.Push(), gui.Button("Launch"), gui.Button("Build From"), gui.Push()],
           [gui.Push()],
@@ -46,7 +41,7 @@ while True:
             continue
         clean_dir = directory[(directory.rfind("/")+1):]
         window["curPath"].update(value=clean_dir)
-        localSamase=Samase(directory, None)
+        localSamase = Samase(directory, None)
     if event == "Select Executable to Unpack":
         directory = gui.popup_get_file('Select path', initial_folder=os.getcwd(), file_types=(("Executable files", "*.exe"),))
         if directory == "":
@@ -54,24 +49,24 @@ while True:
             continue
         clean_dir = directory[(directory.rfind("/")+1):]
         window["curExe"].update(value=clean_dir)
-        unpackSamase=Samase(directory, None)
+        unpackSamase = Samase(directory, None)
     if event == "Launch":
-        if not exists_localSamase(locals()):
+        if not assertSet(localSamase):
             continue
-        localSamase.operation_type = 0
+        localSamase.operation_type = OpType.Run
         localSamase.is64 = values["x64"]
         localSamase.operate()
     if event == "Build From":
-        if not exists_localSamase(locals()):
+        if not assertSet(localSamase):
             continue
-        localSamase.operation_type = 1
+        localSamase.operation_type = OpType.Build
         localSamase.is64 = values["x64"]
         localSamase.operate()
     if event == "Unpack from":
-        if not exists_unpackSamase(locals()):
+        if not assertSet(unpackSamase):
             continue
         assert unpackSamase.folder.endswith(".exe"), f"Expected executable to unpack, instead got {localSamase.folder}"
-        unpackSamase.operation_type = 2
+        unpackSamase.operation_type = OpType.Unpack
         localSamase.is64 = values["x64"]
         unpackSamase.operate()
 
