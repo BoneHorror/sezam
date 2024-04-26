0fec7b2ee937dea502f3e03edd6964d76a7f7ef1
BoneHorror <kilometrkm@gmail.com>
2024-04-26 20:05:50 +0200

    Verbose and more future-proof iteration of window.py, fix unpack using samase.build instead of samase.unpack

--- window.py
+++ window.py
@@ -1,4 +1,3 @@
-# TODO: turn into run.bat and build.bat (or .sh for linux?)
 # python3.12 window.py
 # pyinstaller --onefile window.py
 # pyinstaller window.spec
@@ -6,12 +5,12 @@ import PySimpleGUI as gui
 import logging
 import samase
 import os
-
-LOGFILE = "sezam.log"
+from utilities import log_print
+from const import LOGFILE, SEL_DIR, DIR_HINT, START, BUILD, STR_64, SEL_UNPK, SEL_POPUP, SEL_ERR, UNPK_HINT, WINDOW_NAME, SECTION_1, SECTION_2
 
 def assert_path(path: str | None, message: str = "Attempted to perform an operation without a directory selected!") -> bool:
     if path == "" or path is None:
-        print(message)
+        log_print(message)
         return False
     return True
 
@@ -21,47 +20,55 @@ class Window():
         self.local_samase = ""
         self.unpack_samase = ""
 
-        layout = [[gui.Button("Select build directory"), gui.Text("Please select a path", key="curPath"), gui.Push(), gui.Checkbox("Run in x64", key="x64")],
-                [gui.Push(), gui.Button("Launch"), gui.Button("Build From"), gui.Push()],
+        layout = [[gui.Push(), gui.Text(SECTION_1, font=("Helvetica", 12, "bold")), gui.Push()],
+                [gui.Button(SEL_DIR), gui.Text(DIR_HINT, key="curPath"), gui.Push(), gui.Checkbox(STR_64, key="x64")],
+                [gui.Push(), gui.Button(START), gui.Button(BUILD), gui.Push()],
                 [gui.Push()],
-                [gui.Button("Select .exe to Unpack"), gui.Text("No exe selected", key="curExe")],
-                [gui.Push(), gui.Button("Unpack from"), gui.Push()]
+                [gui.Push(), gui.Text(SECTION_2, font=("Helvetica", 12, "bold"), tooltip="The exe will be unpacked to /target/ in this directory"), gui.Push()],
+                [gui.Button(SEL_UNPK), gui.Text(UNPK_HINT, key="curExe")],
+                [gui.Push(), gui.Button(SEL_UNPK), gui.Push()]
         ]
-        self.window = gui.Window("Sezam", layout, resizable=True)
+        self.window = gui.Window(WINDOW_NAME, layout, resizable=True)
 
     def run(self):
         while True:
             event, values = self.window.read()
+            log_print(f"Executing event: {event}...")
             if event == gui.WIN_CLOSED:
                 break
-            if event == "Select build directory":
-                directory: str | None = gui.popup_get_folder('Select path', initial_folder=os.getcwd())
-                if not assert_path(directory, "Nothing selected"):
+            if event == SEL_DIR:
+                directory: str | None = gui.popup_get_folder(SEL_POPUP, initial_folder=os.getcwd())
+                log_print(f"Selected directory: {directory}")
+                if not assert_path(directory, SEL_ERR):
                     continue
                 clean_dir = directory[(directory.rfind("/")+1):] # TODO: use appropriate function from os.path
                 self.window["curPath"].update(value=clean_dir)
                 self.local_samase = directory
-            if event == "Select .exe to Unpack":
-                directory: str | None = gui.popup_get_file('Select path', initial_folder=os.getcwd(), file_types=(("Executable files", "*.exe"),))
-                if not assert_path(directory, "Nothing selected"):
+            if event == SEL_UNPK:
+                directory: str | None = gui.popup_get_file(SEL_POPUP, initial_folder=os.getcwd(), file_types=(("Executable files", "*.exe"),))
+                log_print(f"Selected executable: {directory}")
+                if not assert_path(directory, SEL_ERR):
                     continue
                 clean_dir = directory[(directory.rfind("/")+1):]
                 self.window["curExe"].update(value=clean_dir)
                 self.unpack_samase = directory
-            if event == "Launch":
+            if event == START:
                 if not assert_path(self.local_samase):
                     continue
+                log_print(f"Trying to launch a session from {self.local_samase}...")
                 samase.run(self.local_samase, values["x64"])
-            if event == "Build From":
+            if event == BUILD:
                 if not assert_path(self.local_samase):
                     continue
+                log_print(f"Trying to pack an executable from {self.local_samase}...")
                 samase.build(self.local_samase, values["x64"])
-            if event == "Unpack from":
+            if event == SEL_UNPK:
                 if not assert_path(self.unpack_samase):
                     continue
-                # on assumption that the user must select .exe file in the popup
-                assert self.unpack_samase.endswith(".exe"), f"Expected executable to unpack, instead got {self.local_samase}"
-                samase.build(self.unpack_samase, values["x64"])
+                if not self.unpack_samase.endswith(".exe"):
+                    log_print(f"Expected executable to unpack, instead got {self.local_samase}!")
+                    continue
+                samase.unpack(self.unpack_samase, values["x64"])
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename=LOGFILE, encoding='utf8', level = logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
